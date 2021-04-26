dfffdf2ab659c5eb3d1f4cb6f33c9653d331fb3d
ptsiakos77@gmail.com <tsiakos@workable.com>
2021-04-26 20:14:46 +0300

    Add 404 custom page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from "./components/Header";
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import Landing from './components/Landing';
 import Login from './components/user/Login';
 import Signup from './components/user/Signup';
@@ -14,6 +14,7 @@ import UpdateTask from './components/task/UpdateTask';
 import TaskDB from './components/task/TaskDB'
 import Settings from './components/user/Settings'
 import EnterQRCode from './components/user/EnterQRCode';
+import NotFound from './components/common/NotFound';
 import { getJWTToken } from './helpers/securityUtils'
 
 const ProtectedRoute = ({ component: Component, ...rest }) => (
@@ -32,20 +33,23 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Header />
-        <Route exact path="/" component={Landing}></Route>
-        <Route exact path="/login" component={Login}></Route>
-        <Route exact path="/enterqrcode" component={EnterQRCode}></Route>
-        <Route exact path="/logout" component={Landing}></Route>
-        <Route exact path="/signup" component={Signup}></Route>
-        <ProtectedRoute exact path="/settings" component={Settings}></ProtectedRoute>
-        <Route exact path="/verifyAccount" component={VerifyAccount}></Route>
-        <ProtectedRoute exact path="/dashboard" component={Dashboard}></ProtectedRoute>
-        <ProtectedRoute exact path="/createProject" component={CreateProject}></ProtectedRoute>
-        <ProtectedRoute exact path="/projects/:id/tasks" component={TaskList}></ProtectedRoute>
-        <ProtectedRoute exact path="/projects/:id/createTask" component={CreateTask}></ProtectedRoute>
-        <ProtectedRoute exact path="/projects/:id/update" component={UpdateProject}></ProtectedRoute>
-        <ProtectedRoute exact path="/tasks/:id/update" component={UpdateTask}></ProtectedRoute>
-        <ProtectedRoute exact path="/tasks/db" component={TaskDB}></ProtectedRoute>
+          <Switch>
+            <Route exact path="/" component={Landing}></Route>
+            <Route exact path="/login" component={Login}></Route>
+            <Route exact path="/enterqrcode" component={EnterQRCode}></Route>
+            <Route exact path="/logout" component={Landing}></Route>
+            <Route exact path="/signup" component={Signup}></Route>
+            <ProtectedRoute exact path="/settings" component={Settings}></ProtectedRoute>
+            <Route exact path="/verifyAccount" component={VerifyAccount}></Route>
+            <ProtectedRoute exact path="/dashboard" component={Dashboard}></ProtectedRoute>
+            <ProtectedRoute exact path="/createProject" component={CreateProject}></ProtectedRoute>
+            <ProtectedRoute exact path="/projects/:id/tasks" component={TaskList}></ProtectedRoute>
+            <ProtectedRoute exact path="/projects/:id/createTask" component={CreateTask}></ProtectedRoute>
+            <ProtectedRoute exact path="/projects/:id/update" component={UpdateProject}></ProtectedRoute>
+            <ProtectedRoute exact path="/tasks/:id/update" component={UpdateTask}></ProtectedRoute>
+            <ProtectedRoute exact path="/tasks/db" component={TaskDB}></ProtectedRoute>
+            <Route component={NotFound}></Route>
+          </Switch>
       </div>
     </BrowserRouter>
   );
---
+++ src/components/common/NotFound.js
@@ -0,0 +1,14 @@
+import './NotFount.css'
+
+export default function NotFound() {
+    return (
+        <div className="row">
+            <div className="col s4">
+            </div>
+            <div className="col s4 center">
+                <h1>Hmm, this page does not exist</h1>
+                <p>Sorry, the page you’re looking for isn’t here. Check if there are any types in the web address</p>
+            </div>
+        </div>
+    )
+}
